047cd931c693c6c0cf3ce9b65758e28b02903d76
André Cambraia <andrempcambraia@gmail.com>
2024-01-24 15:32:32 -0300

    V2

--- script.js
+++ script.js
@@ -12,15 +12,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Condições Iniciais
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Condições Iniciais
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 // Função de Trocar de Jogador
 const switchPlayer = function () {
@@ -87,20 +99,4 @@ btnHold.addEventListener('click', function () {
 });
 
 // Função do botão de Novo Jogo
-btnNew.addEventListener('click', function () {
-  // 1. Resetar as condições iniciais
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  scores[0] = 0;
-  scores[1] = 0;
-  diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-});
+btnNew.addEventListener('click', init);
